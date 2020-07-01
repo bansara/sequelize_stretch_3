03f360b856cbc7a2329bff74c6381f6b18c28d90
Josh Geisler <joshuageisler@gmail.com>
2020-07-01 18:50:09 -0400

    specs passed

--- models/index.js
+++ models/index.js
@@ -2,4 +2,18 @@ const Person = require('./person');
 const Recipe = require('./recipe');
 //YOUR CODE HERE
 
+Recipe.belongsTo(Person);
+Person.hasMany(Recipe);
+
+Person.findWithRecipes = async function () {
+    return await Person.findAll({
+        include: [Recipe]
+    })
+}
+
+Person.prototype.writeRecipe = async function (obj) {
+    const newRecipe = await Recipe.create(obj);
+    this.addRecipe(newRecipe);
+}
+
 module.exports = { Person, Recipe };
